34bd68bd9efb6cc137b737431c3f34bf3d95615b
NgocNB20 <95111259+NgocNB20@users.noreply.github.com>
2024-03-12 21:39:45 +0700

    first commit

---
+++ NBN/src/test/java/com/project/nbn/NbnApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.nbn;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NbnApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
